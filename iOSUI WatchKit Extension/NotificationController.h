f4b07cf7345b32f8ec6f1584a03b7723fad7dc2d
Hirohisa Kobayasi <koba@hogelab.net>
2015-03-30 07:50:05 +0900

    AppleWatch

---
+++ iOSUI WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  iOSUI WatchKit Extension
+//
+//  Created by 小林博久 on 2015/03/29.
+//  Copyright (c) 2015年 khirohis. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
