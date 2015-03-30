092c9dbb16fb1c00ed5f30ff96a6b76b97574cbb
Hirohisa Kobayasi <kobayasi@plala.co.jp>
2015-03-30 21:13:14 +0900

    add glance

---
+++ iOSUI WatchKit Extension/GlanceController.h
@@ -0,0 +1,13 @@
+//
+//  GlanceController.h
+//  iOSUI
+//
+//  Created by 小林 博久 on 2015/03/30.
+//  Copyright (c) 2015年 khirohis. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ iOSUI WatchKit Extension/GlanceController.m
@@ -0,0 +1,25 @@
+//
+//  GlanceController.m
+//  iOSUI
+//
+//  Created by 小林 博久 on 2015/03/30.
+//  Copyright (c) 2015年 khirohis. All rights reserved.
+//
+
+#import "GlanceController.h"
+
+@implementation GlanceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+}
+
+- (void)willActivate {
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    [super didDeactivate];
+}
+
+@end
